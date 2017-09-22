365db7b7896773ba5824f9dc9decfa386f081aa5
Randy Ou <ourd2012@gmail.com>
2017-09-22 19:02:00 +0800

    恢复Dock进度条。

--- src/main/backgroundWindow.js
+++ src/main/backgroundWindow.js
@@ -1,5 +1,5 @@
 import { BrowserWindow } from 'electron'
-// import torrentController from './torrentController'
+import torrentController from './torrentController'
 
 const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080/bg.html`
@@ -16,7 +16,7 @@ export default {
       defaultEncoding: 'utf-8',
       show: false
     })
-    // torrentController.onProgress(backgroundWindow)
+    torrentController.onProgress(backgroundWindow)
     backgroundWindow.loadURL(winURL)
 
     backgroundWindow.on('closed', () => {
